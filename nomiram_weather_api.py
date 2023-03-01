94f76542934d29995ea904eb38cb73c6c1ef5acc
Andrey Mironenko <qwerty.mironenko@gmail.com>
2023-03-01 16:43:10 +0300

    PEP 8

--- nomiram_weather_api.py
+++ nomiram_weather_api.py
@@ -14,14 +14,17 @@ app = Flask(__name__)
 
 geolocation = Nominatim(user_agent="nomiram-app")
 BASE_URL = ""
-PORT = os.environ.get("LISTEN_PORT",5001)
+PORT = os.environ.get("LISTEN_PORT", 5001)
 
 BASE_URL = os.environ.get("API_URL")
+
 
 class APIException(Exception):
     '''Exception that may used when API return not 200 status'''
 
-def get_weather(city:str, timestamp:str = None, current_weather:bool=False) -> str | None:
+
+def get_weather(city: str, timestamp: str = None,
+                current_weather: bool = False) -> str | None:
     """
     Get weather from API
 
@@ -41,8 +44,8 @@ def get_weather(city:str, timestamp:str = None, current_weather:bool=False) -> s
     timezone = tf.timezone_at(lng=location.longitude, lat=location.latitude)
 
     params = {
-        "timezone": timezone, 
-        "latitude": location.latitude, 
+        "timezone": timezone,
+        "latitude": location.latitude,
         "longitude": location.longitude
     }
     if current_weather:
@@ -52,15 +55,17 @@ def get_weather(city:str, timestamp:str = None, current_weather:bool=False) -> s
         date = datetime.datetime.strptime(timestamp, "%Y-%m-%dT%H:%M")
         params["start_date"] = date.strftime("%Y-%m-%d")
         params["end_date"] = date.strftime("%Y-%m-%d")
-        params["hourly"]="temperature_2m"
-    resp = requests.get(BASE_URL,params=params,timeout=10)
+        params["hourly"] = "temperature_2m"
+    resp = requests.get(BASE_URL, params=params, timeout=10)
     if resp.status_code != 200:
         logging.error(resp.text)
         raise APIException(json.loads(resp.text))
 
     return resp.text
 
-def get_temperature(city:str, timestamp:str = None, current_weather:bool=False) -> float | None:
+
+def get_temperature(city: str, timestamp: str = None,
+                    current_weather: bool = False) -> float | None:
     """
     Get temperature from API
 
@@ -73,13 +78,14 @@ def get_temperature(city:str, timestamp:str = None, current_weather:bool=False) 
         float: temperature in Celsius
     """
     resp_t = get_weather(city=city,
-                       current_weather=current_weather,
-                       timestamp=timestamp)
+                         current_weather=current_weather,
+                         timestamp=timestamp)
     if resp_t is None:
         return None
     json_resp = json.loads(resp_t)
     if timestamp:
-        # return temperature by hour (0-23) from hourly temperature (0-24) from API
+        # return temperature by hour (0-23) from hourly temperature (0-24) from
+        # API
         date = datetime.datetime.strptime(timestamp, "%Y-%m-%dT%H:%M")
         logging.debug(json_resp)
         if json_resp.get("hourly", None) is None:
@@ -88,6 +94,7 @@ def get_temperature(city:str, timestamp:str = None, current_weather:bool=False) 
     if current_weather:
         return float(json_resp["current_weather"]["temperature"])
     return None
+
 
 @app.route("/v1/forecast/")
 def v1_get_temperature_forecast():
@@ -100,15 +107,17 @@ def v1_get_temperature_forecast():
     city = request.args.get("city")
     dt = request.args.get("dt")
     if not city or not dt:
-        return jsonify({"error":"city and dt must provided"}), 400
+        return jsonify({"error": "city and dt must provided"}), 400
     temperature = None
     try:
-        temperature = get_temperature(city=city,timestamp=dt)
+        temperature = get_temperature(city=city, timestamp=dt)
     except APIException as e:
-        return jsonify({"error":str(e)}), 500
+        return jsonify({"error": str(e)}), 500
     if temperature is None:
-        return jsonify({"error":"Internal Server Error"}), 500
-    return jsonify({"city": city, "unit": "celsius", "temperature": temperature})
+        return jsonify({"error": "Internal Server Error"}), 500
+    return jsonify({"city": city, "unit": "celsius",
+                   "temperature": temperature})
+
 
 @app.route("/v1/current/")
 def v1_get_temperature_now():
@@ -120,18 +129,20 @@ def v1_get_temperature_now():
     """
     city = request.args.get("city")
     if not city:
-        return jsonify({"error":"city must provided"}), 400
+        return jsonify({"error": "city must provided"}), 400
 
     temperature = None
     try:
-        temperature = get_temperature(city=city,current_weather=True)
+        temperature = get_temperature(city=city, current_weather=True)
     except APIException as e:
-        return jsonify({"error":json.loads(str(e))}), 500
+        return jsonify({"error": json.loads(str(e))}), 500
     if temperature is None:
-        return jsonify({"error":"Internal Server Error"}), 500
-    return jsonify({'city': city, "unit": "celsius", "temperature": temperature})
+        return jsonify({"error": "Internal Server Error"}), 500
+    return jsonify({'city': city, "unit": "celsius",
+                   "temperature": temperature})
+
 
 if __name__ == "__main__":
     # print(get_temperature("Moscow",current_weather=True))
     # print(get_temperature("Moscow",timestamp="2023-02-17T13:00"))
-    app.run("0.0.0.0",port=PORT)
+    app.run("0.0.0.0", port=PORT)
