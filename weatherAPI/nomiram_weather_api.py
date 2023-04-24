729db09f7dfaf8e926068a01b5e8de077b036068
Andrey Mironenko <qwerty.mironenko@gmail.com>
2023-04-25 01:28:51 +0300

    feature add redis temperature cache

--- weatherAPI/nomiram_weather_api.py
+++ weatherAPI/nomiram_weather_api.py
@@ -1,4 +1,5 @@
 '''Get weather from API'''
+
 import datetime
 import json
 import logging
@@ -12,6 +13,7 @@ from auth_pb2 import AuthRequest
 from flask import Flask, jsonify, request
 from geopy.geocoders import Nominatim
 from timezonefinder import TimezoneFinder
+import redis
 
 logging.basicConfig(level=logging.DEBUG)
 app = Flask(__name__)
@@ -19,6 +21,7 @@ app = Flask(__name__)
 geolocation = Nominatim(user_agent="nomiram-app")
 PORT = os.environ.get("LISTEN_PORT", 5001)
 
+FEATURE_REDIS = bool(os.getenv("REDIS_SERVER"))
 BASE_URL = os.environ.get("API_URL")
 AUTH_HEADER = 'Own-Auth-UserName'
 
@@ -97,23 +100,40 @@ def get_temperature(city: str, timestamp: str = None,
     Returns:
         float: temperature in Celsius
     """
+    date = datetime.datetime.now()
+    if timestamp:
+        date = datetime.datetime.strptime(timestamp, "%Y-%m-%dT%H:%M")
+
+    get_date = date.strftime("%Y-%m-%dT%H:00")
+    if FEATURE_REDIS:
+        req = requests.get(f"http://localhost:{PORT}/v1/redis/",
+                           params={"key": get_date}, timeout=200)
+        if req.status_code == 200:
+            logging.debug("cache found in redis: %s, %s",
+                          req.status_code, req.text)
+            return float(req.json()["value"])
+        logging.debug("not found in redis: %s, %s", req.status_code, req.text)
     resp_t = get_weather(city=city,
                          current_weather=current_weather,
                          timestamp=timestamp)
     if resp_t is None:
         return None
     json_resp = json.loads(resp_t)
+    temperature = None
     if timestamp:
         # return temperature by hour (0-23) from hourly temperature (0-24) from
         # API
-        date = datetime.datetime.strptime(timestamp, "%Y-%m-%dT%H:%M")
         logging.debug(json_resp)
         if json_resp.get("hourly", None) is None:
             return None
-        return float(json_resp["hourly"]["temperature_2m"][date.hour])
+        temperature = float(json_resp["hourly"]["temperature_2m"][date.hour])
     if current_weather:
-        return float(json_resp["current_weather"]["temperature"])
-    return None
+        temperature = float(json_resp["current_weather"]["temperature"])
+    if FEATURE_REDIS:
+        req_set = requests.put(f"http://localhost:{PORT}/v1/redis/",
+                               json={"key": get_date, "value": temperature}, timeout=200)
+        logging.debug("redis ans: %s %s", req_set.status_code, req_set.text)
+    return temperature
 
 
 @app.route("/v1/forecast/")
@@ -170,6 +190,49 @@ def v1_get_temperature_now():
         return jsonify({"error": "Internal Server Error"}), 500
     return jsonify({'city': city, "unit": "celsius",
                    "temperature": temperature})
+
+
+@app.route("/v1/redis/", methods=["PUT"])
+def v1_redis_set_data():
+    """
+    This function puts data into a Redis database using a provided key-value pair.
+
+    Returns:
+      If the "key" and "value" are provided in the JSON data and the Redis server is successfully
+    connected, the function will return a JSON object with the message {"set": "OK"}. If either the
+    "key" or "value" is missing from the JSON data or the Redis server cannot be connected, 
+    the function will return a JSON object with an error message {"error": str} and a 500 status
+    """
+    json_data = request.json
+    key = json_data.get("key", None)
+    value = json_data.get("value", None)
+    if key is None or value is None:
+        return jsonify({"error": 'json {"key"=key,"value"=value} must provided'}), 500
+    r = redis.Redis(host=os.getenv("REDIS_SERVER"), port=os.getenv(
+        "REDIS_PORT"), decode_responses=True, password=os.getenv("REDIS_PASSWORD"))
+    if r.set(key, value):
+        return jsonify({"set": "OK"})
+    return jsonify({"error": 'can`t connect to redis'}), 500
+
+
+@app.route("/v1/redis/", methods=["GET"])
+def v1_redis_get_data():
+    """
+    This function retrieves data from a Redis server based on a given key and returns the value if
+    it exists, or a "key not found" message if it does not.
+
+    Returns:
+      If the value of the key exists in Redis, a JSON object {"value": value}. 
+    If the key does not exist, a JSON object with the message {"OK": 'key not found'} 
+    and a 404 status code is returned.
+    """
+    key = request.args.get("key", None)
+    r = redis.Redis(host=os.getenv("REDIS_SERVER"), port=os.getenv(
+        "REDIS_PORT"), decode_responses=True, password=os.getenv("REDIS_PASSWORD"))
+    value = r.get(key)
+    if value is not None:
+        return jsonify({"value": value})
+    return jsonify({"OK": 'key not found'}), 404
 
 
 if __name__ == "__main__":
